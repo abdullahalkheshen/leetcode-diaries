1a3cd04481680c752e3455a76c0900cc37cbb8b8
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2023-12-13 21:22:15 +0200

    Review 76.minimum_window_substring.cpp

--- Design-Gurus/Sliding Window/76.minimum_window_substring.cpp
+++ Design-Gurus/Sliding Window/76.minimum_window_substring.cpp
@@ -26,6 +26,8 @@
     Explanation: No substring in the given string has all characters of the pattern
 */
 
+// ----------------------------- Sliding Window  -------------------------------- //
+
 using namespace std;
 #include <iostream>
 #include <string>
@@ -72,5 +74,126 @@ class Solution
             }
         }
         return (minimum_window > s.size()) ? "" : s.substr(min_window_start, minimum_window);
+    }
+};
+
+
+// ----------------------------- Sliding Window 1 -------------------------------- //
+
+class Solution {
+public:
+    std::string minWindow(std::string s, std::string t) 
+    {
+        if (s.length() == 0 || t.length() == 0) 
+        {
+            return "";
+        }
+
+        std::unordered_map<char, int> dictT;
+        for (char c : t) 
+        {
+            dictT[c]++;
+        }
+
+        int required = dictT.size();
+        int l = 0, r = 0;
+        int formed = 0;
+        int ans[3] = { -1, 0, 0 };
+        std::unordered_map<char, int> windowCounts;
+
+        while (r < s.length()) 
+        {
+            char c = s[r];
+            windowCounts[c]++;
+
+            if (dictT.find(c) != dictT.end() && windowCounts[c] == dictT[c]) 
+            {
+                formed++;
+            }
+
+            while (l <= r && formed == required) 
+            {
+                c = s[l];
+                if (ans[0] == -1 || r - l + 1 < ans[0]) 
+                {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                windowCounts[c]--;
+
+                if (dictT.find(c) != dictT.end() && windowCounts[c] < dictT[c]) 
+                {
+                    formed--;
+                }
+
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substr(ans[1], ans[2] - ans[1] + 1);
+    }
+};
+
+// ----------------------------- Sliding Window 2 -------------------------------- //
+
+class Solution {
+public:
+    std::string minWindow(std::string s, std::string t) 
+    {
+        if (s.length() == 0 || t.length() == 0) 
+        {
+            return "";
+        }
+
+        std::unordered_map<char, int> dictT;
+        for (char c : t) 
+        {
+            dictT[c]++;
+        }
+
+        int required = dictT.size();
+        int l = 0, r = 0;
+        int formed = 0;
+        int ans[3] = { -1, 0, 0 };
+        std::unordered_map<char, int> windowCounts;
+
+        while (r < s.length()) 
+        {
+            char c = s[r];
+            windowCounts[c]++;
+
+            if (dictT.find(c) != dictT.end() && windowCounts[c] == dictT[c]) 
+            {
+                formed++;
+            }
+
+            while (l <= r && formed == required) 
+            {
+                c = s[l];
+                if (ans[0] == -1 || r - l + 1 < ans[0]) 
+                {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                windowCounts[c]--;
+
+                if (dictT.find(c) != dictT.end() && windowCounts[c] < dictT[c]) 
+                {
+                    formed--;
+                }
+
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substr(ans[1], ans[2] - ans[1] + 1);
     }
 };
